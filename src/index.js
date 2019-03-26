b14eb4e36bccc2e49dd38b850050bea8ea3aa296
EricFreireSouza <ericfreire@gmail.com>
2019-03-26 14:08:44 -0300

    Modificação do css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './views/App/App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { urls } from './utils/urlUtils';
