7897e6774ecfc1f30cdda6593bd72f0ccd4291a9
EricFreireSouza <ericfreire@gmail.com>
2019-05-20 11:42:37 -0300

    Updates

---
+++ precache-manifest.4e47d42297061f39a5e3bc32d96f9fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf571d4cf2a4c4b2df823a27852a7d5",
+    "url": "/estacio/static/media/movie.3cf571d4.mp4"
+  },
+  {
+    "revision": "a83434b9966f9e95a8f9",
+    "url": "/estacio/static/js/runtime~main.36d7c9a6.js"
+  },
+  {
+    "revision": "d312f3b118b15906a8e0",
+    "url": "/estacio/static/js/main.85dbbee6.chunk.js"
+  },
+  {
+    "revision": "e71b555be1e2535201bf",
+    "url": "/estacio/static/js/2.b988ba2a.chunk.js"
+  },
+  {
+    "revision": "d312f3b118b15906a8e0",
+    "url": "/estacio/static/css/main.a1e349b8.chunk.css"
+  },
+  {
+    "revision": "3de0e1e1ce9e04fd93bf1218210d2802",
+    "url": "/estacio/index.html"
+  }
+];
